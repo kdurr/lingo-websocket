0963b613868284cd634f050e6debde68537d0a4e
Charles Gould <charles.r.gould@gmail.com>
2017-01-04 19:56:41 -0500

    Configure WebSocket thread pools

--- server/src/main/java/lingo/server/WebSocketConfig.java
+++ server/src/main/java/lingo/server/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package lingo.server;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,18 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		// Single thread unless/until more threads are needed
+		registration.taskExecutor().corePoolSize(1);
+	}
+
+	@Override
+	public void configureClientOutboundChannel(ChannelRegistration registration) {
+		// Single thread unless/until more threads are needed
+		registration.taskExecutor().corePoolSize(1);
+	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
